0716227d48473f72e7ee4b0785ad1ba5fb3308d9
WoojinJungS <woojin.jung@stonybrook.edu>
2024-02-03 09:05:13 +0900

    First working prototype

--- note/src/main/java/com/command/note/InputController.java
+++ note/src/main/java/com/command/note/InputController.java
@@ -1,15 +1,14 @@
 package com.command.note;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-
 @RestController
-public class ProductServiceController {
+@CrossOrigin
+public class InputController {
 
-    @CrossOrigin(origins = "http://localhost:8000")
     @PostMapping("/command")
     public String processCommand(@RequestBody String command) {
         // Process the command (you can add your logic here)
